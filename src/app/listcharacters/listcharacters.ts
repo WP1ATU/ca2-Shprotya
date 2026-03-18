adff0c4b4197252d7e9792fdca9eadc38217e4dc
Lisa <kareevaliza@gmail.com>
2026-03-18 21:32:39 +0000

    lits characters code written

--- src/app/listcharacters/listcharacters.ts
+++ src/app/listcharacters/listcharacters.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Itemsapiservice } from '../itemsapiservice';
 
 @Component({
   selector: 'app-listcharacters',
-  imports: [],
+  imports: [CommonModule],  // add this
   templateUrl: './listcharacters.html',
   styleUrl: './listcharacters.css',
 })
 export class Listcharacters {
+  private _itemsApiService = inject(Itemsapiservice);
+  characters = this._itemsApiService.characters; // expose signal to template
 
+  constructor() {
+    this._itemsApiService.getItems();
+  }
 }
