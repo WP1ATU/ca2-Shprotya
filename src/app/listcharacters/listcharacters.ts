687751cc2f925cd3fa9c07dff37e5d30df9bbc54
Lisa <kareevaliza@gmail.com>
2026-03-18 23:43:10 +0000

    delete button is working now

--- src/app/listcharacters/listcharacters.ts
+++ src/app/listcharacters/listcharacters.ts
@@ -4,15 +4,19 @@ import { Itemsapiservice } from '../itemsapiservice';
 
 @Component({
   selector: 'app-listcharacters',
-  imports: [CommonModule],  // add this
+  imports: [CommonModule],
   templateUrl: './listcharacters.html',
   styleUrl: './listcharacters.css',
 })
 export class Listcharacters {
   private _itemsApiService = inject(Itemsapiservice);
-  characters = this._itemsApiService.characters; // expose signal to template
+  characters = this._itemsApiService.characters;
 
   constructor() {
     this._itemsApiService.getItems();
+  }
+
+  delete(id: any) {
+    this._itemsApiService.deleteItem(id);
   }
 }
